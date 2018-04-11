2819c285ccd4079fdbf7f7f775edeece3e9dad50
ChrisGCampbell <chris@chrisgcampbell.com>
2018-04-11 12:18:26 -0400

    Added action and actioncreator to BookList reducter

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators} from 'redux';
 
 class BookList extends Component {
+
     renderList() {
         return this.props.books.map((book) => {
-            return (
-              <li key={book.title} className="list-group-item">{book.title}</li>
-            );
+           return (
+               <li
+                   key={book.title}
+                   onClick={() => this.props.selectBook(book)}
+                   className="list-group-item">
+                   {book.title}
+               </li>
+           );
         });
     }
 
     render () {
+
         return (
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -21,8 +30,17 @@ class BookList extends Component {
 
 function mapStateToProps(state) {
     return {
+        //returns an object to props inside of BookList
         books: state.books
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will end up as props
+// on the Booklist container
+function mapDispatchToProps(dispatch) {
+    //Whenever selectBook is called, the result should be passed
+    // to all reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
